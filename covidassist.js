9f1668f0f87f1d3e158409f66b6ee2f8d781db8e
Vasu Vijayvargiya <56105511+Vasuvj24@users.noreply.github.com>
2021-05-26 19:04:03 +0530

    Create covidassist.js

---
+++ covidassist.js
@@ -0,0 +1,95 @@
+var i = 0;
+function qsa(selector) {
+  // we can input query selector here and input the class
+  return document.querySelectorAll(selector);
+}
+function id(id) {
+  return document.getElementById(id);
+}
+console.log(menu1);
+id("menu1").addEventListener("click", function () {
+  id("sidebar1").style.left = "0";
+  id("overlay").style.backgroundColor = "rgba(0,0,0,0.6)";
+  id("sidebar1").style.backgroundcp;
+  // id("main").style.filter="blur(5px)";
+  // id("sidebar1").style.filter="blur(0px)";
+});
+console.log(foodbtn1);
+id("foodbtn1").addEventListener("click", function () {
+  i++;
+  if (i % 2 != 0) {
+    console.log(i);
+    id("food1").style.display = "block";
+    // id("sidebar1").style.left="-20vw";
+  } else {
+    console.log(i);
+    id("food1").style.display = "none";
+  }
+});
+id("bedsbtn1").addEventListener("click", function () {
+  i++;
+  if (i % 2 != 0) {
+    console.log(i);
+    id("beds1").style.display = "block";
+  } else {
+    console.log(i);
+    id("beds1").style.display = "none";
+  }
+});
+id("injectionbtn1").addEventListener("click", function () {
+  i++;
+  if (i % 2 != 0) {
+    console.log(i);
+    id("injection1").style.display = "block";
+  } else {
+    console.log(i);
+    id("injection1").style.display = "none";
+  }
+});
+id("closingsidebar").addEventListener("click", function () {
+  id("sidebar1").style.left = "-20vw";
+  id("overlay").style.backgroundColor = "rgba(0,0,0,0)";
+});
+let query = window.matchMedia('(min-width: 320px)');
+if (query.matches) {
+  id("menu1").addEventListener("click", function () {
+    id("sidebar1").style.left = "0";
+    // id("overlay").style.filter="blur(10px)"
+    // id("main").style.filter="blur(5px)";
+  });
+  id("closingsidebar").addEventListener("click", function () {
+    id("sidebar1").style.left = "-50vw";
+  });
+  console.log(foodbtn1);
+  id("foodbtn1").addEventListener("click", function () {
+    i++;
+    if (i % 2 != 0) {
+      console.log(i);
+      id("food1").style.display = "block";
+      // id("sidebar1").style.left="-20vw";
+    } else {
+      console.log(i);
+      id("food1").style.display = "none";
+    }
+  });
+  id("bedsbtn1").addEventListener("click", function () {
+    i++;
+    if (i % 2 != 0) {
+      console.log(i);
+      id("beds1").style.display = "block";
+    } else {
+      console.log(i);
+      id("beds1").style.display = "none";
+    }
+  });
+  id("injectionbtn1").addEventListener("click", function () {
+    i++;
+    if (i % 2 != 0) {
+      console.log(i);
+      id("injection1").style.display = "block";
+    } else {
+      console.log(i);
+      id("injection1").style.display = "none";
+    }
+  });
+}
